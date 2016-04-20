30251927835d7d803a70570daf69342fd75c6c0b
Kevin Fox <kevinmatthewfox@email.arizona.edu>
2016-04-20 16:03:56 -0700

    created check_health files
    instead of having the function in main
    includes check_health.cpp and check_health.h

---
+++ SLAM_v3/src/check_health.cpp
@@ -0,0 +1,43 @@
+// 
+// File name: checkHealth.cpp
+// Author: Kevin Fox
+// Senior Engineering Design 498
+// University of Arizona
+// Team 15017
+// Autonomous Mapping
+//
+
+#include "rplidar.h"
+#include "checkHealth.h"
+
+using namespace rp::standalone::rplidar; //rplidar namespace
+
+bool checkRPLIDARHealth(RPlidarDriver * drv)
+{
+    u_result     op_result;
+    rplidar_response_device_health_t healthinfo;
+
+
+    op_result = drv->getHealth(healthinfo);
+    if (IS_OK(op_result)) // the macro IS_OK is the preperred way to judge whether the operation is succeed.
+	{ 
+        printf("RPLidar health status : %d\n", healthinfo.status);
+        if (healthinfo.status == RPLIDAR_STATUS_ERROR) 
+		{
+            fprintf(stderr, "Error, rplidar internal error detected. Please reboot the device to retry.\n");
+            // enable the following code if you want rplidar to be reboot by software
+            // drv->reset();
+            return false;
+        } 
+		else 
+		{
+            return true;
+        }
+
+    } 
+	else 
+	{
+        fprintf(stderr, "Error, cannot retrieve the lidar health code: %x\n", op_result);
+        return false;
+    }
+}
---
+++ SLAM_v3/src/check_health.h
@@ -0,0 +1,14 @@
+// 
+// File name: checkHealth.h
+// Author: Kevin Fox
+// Senior Engineering Design 498
+// University of Arizona
+// Team 15017
+// Autonomous Mapping
+//
+
+#include "rplidar.h"
+
+using namespace rp::standalone::rplidar; //rplidar namespace
+
+bool checkRPLIDARHealth(RPlidarDriver *drv);
