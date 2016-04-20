547da125899ffff5997c4f2530891cd97b73e38b
Kevin Fox <kevinmatthewfox@email.arizona.edu>
2016-04-20 15:04:01 -0700

    formatting main.cpp

--- SLAM_v3/src/main.cpp
+++ SLAM_v3/src/main.cpp
@@ -125,18 +125,24 @@ bool checkRPLIDARHealth(RPlidarDriver * drv)
 
 
     op_result = drv->getHealth(healthinfo);
-    if (IS_OK(op_result)) { // the macro IS_OK is the preperred way to judge whether the operation is succeed.
+    if (IS_OK(op_result)) // the macro IS_OK is the preperred way to judge whether the operation is succeed.
+	{ 
         printf("RPLidar health status : %d\n", healthinfo.status);
-        if (healthinfo.status == RPLIDAR_STATUS_ERROR) {
+        if (healthinfo.status == RPLIDAR_STATUS_ERROR) 
+		{
             fprintf(stderr, "Error, rplidar internal error detected. Please reboot the device to retry.\n");
             // enable the following code if you want rplidar to be reboot by software
             // drv->reset();
             return false;
-        } else {
+        } 
+		else 
+		{
             return true;
         }
 
-    } else {
+    } 
+	else 
+	{
         fprintf(stderr, "Error, cannot retrieve the lidar health code: %x\n", op_result);
         return false;
     }
